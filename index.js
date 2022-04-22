252f8c7bc35e94d8610e1fe4fed919b0ee94f82e
Denis <dennik1981@Gmail.com>
2022-04-22 13:44:25 +0300

    test

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const SIZE_BLOCK = 35;
+const SIZE_BLOCK = 30;
 
 // механика
 
@@ -20,10 +20,10 @@ const game = {
 		['o','o','o','o','o','o','o','o','o','o'],
 		['o','o','o','o','o','o','o','o','o','o'],
 		['o','o','o','o','o','o','o','o','o','o'],
-		['o','o','x','o','o','o','o','o','o','o'],
-		['o','o','x','o','o','o','o','o','o','x'],
-		['x','x','x','x','o','o','x','x','o','x'],
-		['x','x','x','x','x','x','x','x','x','x'],
+		['o','o','o','o','o','o','o','o','o','o'],
+		['o','o','o','o','o','o','o','o','o','o'],
+		['o','o','o','o','o','o','o','o','o','o'],
+		['$','$','$','$','o','o','o','o','o','o'],
 	],
 
 	activeTetromino: {
@@ -32,23 +32,69 @@ const game = {
 		block: [
 			['o', 'x', 'x'],
 			['o', 'x', 'o'],
-			['0', 'x', 'o'],
+			['o', 'x', 'o'],
+		],
+		rotationIndex: 0,
+		rotation: [
+			[
+				['o','x','o'],
+				['o','x','o'],
+				['x','x','o'],
+			],
+			[
+				['x','o','o'],
+				['x','x','x'],
+				['o','o','o'],
+			],
+			[
+				['o','x','x'],
+				['o','x','o'],
+				['o','x','o'],
+			],
+			[
+				['o','o','o'],
+				['x','x','x'],
+				['o','o','x'],
+			],
 		]
 	},
 	
 	moveLeft() {
-		this.activeTetromino.x -= 1
+		if (this.checkOutPosition(this.activeTetromino.x - 1, this.activeTetromino.y)) {
+			this.activeTetromino.x -= 1
+		}
 	},
 
 	moveRight() {
-		this.activeTetromino.x += 1
+		if (this.checkOutPosition(this.activeTetromino.x + 1, this.activeTetromino.y)) {
+			this.activeTetromino.x += 1
+		}
 	},
 
 	moveDown() {
-		this.activeTetromino.y += 1
+		if (this.checkOutPosition(this.activeTetromino.x, this.activeTetromino.y + 1)) {
+			this.activeTetromino.y += 1
+		} else {
+			this.stopMove();
+		}
 	},
 
 	rotateTetromino() {
+		this.activeTetromino.rotationIndex =  
+			this.activeTetromino.rotationIndex < 3 ?
+				this.activeTetromino.rotationIndex + 1 : 0;
+
+		this.activeTetromino.block = 
+			this.activeTetromino.rotation[this.activeTetromino.rotationIndex];
+		
+		if(!this.checkOutPosition(this.activeTetromino.x, this.activeTetromino.y)) {
+			this.activeTetromino.rotationIndex =  
+			this.activeTetromino.rotationIndex > 0 ?
+				this.activeTetromino.rotationIndex - 1: 3;
+
+			this.activeTetromino.block = 
+			this.activeTetromino.rotation[this.activeTetromino.rotationIndex];
+		}
 	},
 
 	get viewArea() {
@@ -58,37 +104,47 @@ const game = {
 		for (let i = 0; i < tetromino.length; i++) {
 			const row = tetromino[i];
 			for (let j = 0; j < row.length; j++) {
-				if(row[j] === 'x') {
+				if(row[j] !== 'o') {
 					area[y + i][x + j] = tetromino[i][j];
 				}
 			}
 		}
+
 		return area;
+	},
+
+	checkOutPosition(x, y) {
+		const tetromino = this.activeTetromino.block;
+		for (let i = 0; i < tetromino.length; i++) {
+			for (let j = 0; j < tetromino[i].length; j++) {
+				if (tetromino[i][j] === 'o') continue;
+
+				if (!this.area[y + i] ||
+					!this.area[y + i][x + j] ||
+					this.area[y + i][x + j] !== 'o') { 
+					return false
+				}
+			}
+		}
+		return true
+	},
+
+	stopMove() {
+		const {x, y, block: tetromino} = this.activeTetromino;
+
+		for (let i = 0; i < tetromino.length; i++) {
+			const row = tetromino[i];
+			for (let j = 0; j < row.length; j++) {
+				if(row[j] !== 'o') {
+					this.area[y + i][x + j] = tetromino[i][j];
+				}
+			}
+		}
 	}
 }
 
-game.moveDown();
-game.moveDown();
-game.moveDown();
-game.moveDown();
-game.moveDown();
-game.moveDown();
-game.moveDown();
-game.moveDown();
-game.moveDown();
-game.moveDown();
-game.moveDown();
-game.moveDown();
-game.moveRight();
-game.moveRight();
-game.moveRight();
-game.moveRight();
-game.moveDown();
-game.moveDown();
-game.moveDown();
-
-
 // отрисовка
+
 const container = document.querySelector('.container');
 const canvas = document.createElement('canvas');
 canvas.classList.add('game-area');
@@ -98,13 +154,25 @@ canvas.height = SIZE_BLOCK * 20;
 
 const context = canvas.getContext('2d');
 
+// домашнее задание Напишите обработчик события по нажатию на Enter, чтобы начать игру
+// Только после нажатия на Enter появляется игровое поле и можно двигать фигуры
+
+const btnEnter = document.querySelector('.btn__enter');
+
+btnEnter.addEventListener('click', () => {
+	container.style.display = 'block';
+});
+// ......................................................
+
 const showArea = area => {
+	context.clearRect(0, 0, canvas.width, canvas.height)
+
 	for (let y = 0; y < area.length; y++) {
 		const line = area[y];
 
 		for (let x = 0; x < line.length; x++) {
 			const block = line[x];
-			if(block === 'x') {
+			if(block !== 'o') {
 				context.fillStyle = 'green';
 				context.strokeStyle = 'gold';
 				context.fillRect(x * SIZE_BLOCK , y * SIZE_BLOCK, SIZE_BLOCK, SIZE_BLOCK);
@@ -113,5 +181,28 @@ const showArea = area => {
 		}
 	}
 };
+
+window.addEventListener('keydown', event => {
+	const key = event.code;
+	
+	switch (key) {
+		case 'ArrowLeft':
+			game.moveLeft();
+			showArea(game.viewArea);
+		break;
+		case 'ArrowRight':
+			game.moveRight();
+			showArea(game.viewArea);
+		break;
+		case 'ArrowDown':
+			game.moveDown();
+			showArea(game.viewArea);
+		break;
+		case 'ArrowUp':
+			game.rotateTetromino();
+			showArea(game.viewArea);
+		break;
+	}
+});
 
 showArea(game.viewArea);
